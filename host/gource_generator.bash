b4003b0041b2b0d4338852a29aa3aa66033367d2
Taivo Käsper <taivo.kasper@gmail.com>
2015-01-09 05:57:16 +0200

    Improved finish detection

--- host/gource_generator.bash
+++ host/gource_generator.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e # stop on first error
 
 RESULTS_DIR=$1
 FILENAME=$2
@@ -7,21 +6,24 @@ TITLE_TEXT=$3
 
 rm -f ${RESULTS_DIR}/${FILENAME}*
 
-screen -dmS recording xvfb-run -a -s "-screen 0 1280x720x24" gource -1280x720 -r 30 --title "$TITLE_TEXT" --user-image-dir /avatars/ --highlight-all-users -s 0.5 -o ${RESULTS_DIR}/${FILENAME}.ppm
+screen -dmS "recording $FILENAME" xvfb-run -a -s "-screen 0 1280x720x24" gource -1280x720 -r 30 --title "$TITLE_TEXT" --user-image-dir /avatars/ --highlight-all-users -s 0.5 -o ${RESULTS_DIR}/${FILENAME}.ppm
 
-# This hack is needed because gource process doesn't stop
+# This hack is needed because gource process doesn't stop by itself
 sleep 5
 
 lsof | grep ${RESULTS_DIR}/${FILENAME}.ppm
 greprc=$?
-
-while [[ "$greprc" != "0" ]] ;
+while [[ "$greprc" -eq "0" ]] ;
 do
+	echo "File is open by another process. Waiting for the process to finish"
 	sleep 10
-	echo "File is open by a process. Wait for the processing to finish"
+	lsof | grep ${RESULTS_DIR}/${FILENAME}.ppm
+	greprc=$?
 done
+echo "Recording finished. Starting to process ppm to mp4"
 
-screen -S recording -X quit
+screen -S "recording $FILENAME" -X quit
 
 xvfb-run -a -s "-screen 0 1280x720x24" ffmpeg -y -r 30 -f image2pipe -loglevel info -vcodec ppm -i ${RESULTS_DIR}/${FILENAME}.ppm -vcodec libx264 -preset medium -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ${RESULTS_DIR}/${FILENAME}
+
 rm -f ${RESULTS_DIR}/${FILENAME}.ppm
