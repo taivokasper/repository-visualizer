c833e4fc7124fdc7bbb8745600e68b04343d8b35
Taivo Käsper <taivo.kasper@gmail.com>
2015-01-15 01:05:22 +0200

    Unnecessary command

--- update-and-start.sh
+++ update-and-start.sh
@@ -3,8 +3,7 @@ set -e
 
 git pull origin master
 
-fig -f fig-build.yml stop && \
-    fig -f fig-build.yml rm --force && \
+fig -f fig-build.yml rm --force && \
     fig -f fig-build.yml up && \
     fig -f fig-build.yml rm --force
 
